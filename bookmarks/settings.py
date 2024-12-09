9d7ae3229d615e98dad894bc736f93670d183a17
451suman <sumanmushyakhwo@gmail.com>
2024-12-09 13:53:56 +0545

    auth ko kaam saskyo

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
